37dd432889a43021d2463f8060f08cc4f6c37f43
Benjamin Schachter <benschac@gmail.com>
2022-09-27 18:53:01 -0400

    remove sample code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
 import "./App.css";
-import Ballot from "./Components/Ballot/Ballot";
-import { useGetBallots } from "./Hooks/useGetBallots";
 
 const App: React.FC = () => {
-  const { ballots } = useGetBallots();
   return (
     <div>
-      <header>
-        <img
-          src={
-            "https://www.dailypay.com/wp-content/uploads/DailyPay-Logo-White.svg"
-          }
-          alt="logo"
-        />
-      </header>
-
-      <ul>
-        {ballots?.items.map((ballot) => (
-          <li key={ballot.id}>
-            {ballot.items.map((item) => (
-              <div key={item.id}>
-                <img alt={item.id} src={item.photoUrL} />
-                {item.title}
-              </div>
-            ))}
-          </li>
-        )) ?? "loading"}
-      </ul>
-      <Ballot />
+      <h1>Movie Ballot Vote</h1>
     </div>
   );
 };

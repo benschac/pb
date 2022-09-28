76ec074767bafa6708571058bf3660e3defad9be
Benjamin Schachter <benschac@gmail.com>
2022-09-27 20:03:05 -0400

    remove nav

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,26 +1,9 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { useGetBallots } from "../Hooks/useGetBallots";
+import { Outlet } from "react-router-dom";
 
 const MainLayout: React.FC = () => {
-  const { ballots } = useGetBallots();
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          {ballots?.items.map(
-            (cat) =>
-              (
-                <li key={cat.id}>
-                  <Link to={`/category/${cat.id}`}>{cat.title}</Link>
-                </li>
-              ) ?? "loading"
-          )}
-        </ul>
-      </nav>
       <main>
         <Outlet />
       </main>
